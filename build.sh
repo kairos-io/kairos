01b8eec3e7cfc9b1cd0357bee38d0c14b60bea4b
Ettore Di Giacinto <mudler@sabayon.org>
2022-01-27 22:35:52 +0100

    Split build args and Dockerfiles

--- build.sh
+++ build.sh
@@ -4,8 +4,15 @@ set -ex
 
 IMAGE="${IMAGE:-c3os}"
 ISO="${ISO:-c3os}"
+FLAVOR="${FLAVOR:-opensuse}"
+C3OS_VERSION="${C3OS_VERSION:-c3OS22}"
+K3S_VERSION="${K3S_VERSION:-v1.21.4+k3s1}"
 
-docker build -t $IMAGE .
+docker build --build-arg C3OS_VERSION=$C3OS_VERSION \
+             --build-arg K3S_VERSION=$K3S_VERSION \
+             -t $IMAGE \
+             -f Dockerfile.${FLAVOR} ./
+
 docker run -v $PWD:/cOS \
            -v /var/run:/var/run \
            --entrypoint /usr/bin/luet-makeiso \
