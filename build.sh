c5603b6df004843c66f2708f47831f976d870264
Ettore Di Giacinto <mudler@sabayon.org>
2022-02-16 18:35:06 +0100

    :penguin: Add ubuntu/fedora flavors
    Fixes #7

--- build.sh
+++ build.sh
@@ -10,9 +10,10 @@ C3OS_VERSION="${C3OS_VERSION:--c3OS27}"
 K3S_VERSION="${K3S_VERSION:-v1.21.4+k3s1}"
 OS_LABEL="${OS_LABEL:-$FLAVOR-latest}"
 OS_NAME="${OS_NAME:-$OS_ID-$FLAVOR}"
-
+LUET_VERSION="${LUET_VERSION:-0.22.7-1}"
 docker build --build-arg C3OS_VERSION=$C3OS_VERSION \
              --build-arg K3S_VERSION=$K3S_VERSION \
+             --build-arg LUET_VERSION=$LUET_VERSION \
              --build-arg OS_LABEL=$OS_LABEL \
              --build-arg OS_NAME=$OS_NAME \
              -t $IMAGE \
