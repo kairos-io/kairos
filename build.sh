c788cc3707dfb7fb91eefd6460252b3aa7314823
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-04-29 19:45:24 +0200

    :robot: pinpoint toolchain image

--- build.sh
+++ build.sh
@@ -15,4 +15,4 @@ docker build --build-arg C3OS_VERSION=$C3OS_VERSION \
 docker run -v $PWD:/cOS \
            -v /var/run:/var/run \
            --entrypoint /usr/bin/luet-makeiso \
-           -i --rm quay.io/costoolkit/toolchain ./iso.yaml --image $IMAGE --output $ISO
+           -i --rm quay.io/costoolkit/toolchain:0.8.7-16-gdcaac339 ./iso.yaml --image $IMAGE --output $ISO
