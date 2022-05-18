e340cfa8b7db07947471ac4595c9f47728dc9423
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-05-18 20:53:52 +0000

    :robot: Add qemu_run helper script

---
+++ qemu_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -e disk.img ]; then
+    qemu-img create -f qcow2 disk.img 40g
+fi
+
+qemu-system-x86_64 \
+    -m ${MEMORY:=2096} \
+    -smp cores=2 \
+    -nographic \
+    -serial mon:stdio \
+    -rtc base=utc,clock=rt \
+    -chardev socket,path=qga.sock,server,nowait,id=qga0 \
+    -device virtio-serial \
+    -device virtserialport,chardev=qga0,name=org.qemu.guest_agent.0 \
+    -drive if=virtio,media=disk,file=disk.img \
+    -drive if=ide,media=cdrom,file=${1:-c3os.iso}
