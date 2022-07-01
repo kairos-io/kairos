11158a12aa13b5181f9c16a5718db63b89a2f40b
Olaf Meeuwissen <paddy-hack@member.fsf.org>
2022-07-02 03:06:45 +0900

    Support builds from behind a proxy (#35)

--- build.sh
+++ build.sh
@@ -11,11 +11,15 @@ docker build --build-arg C3OS_VERSION=$C3OS_VERSION \
              --build-arg LUET_VERSION=$LUET_VERSION \
              --build-arg OS_LABEL=$OS_LABEL \
              --build-arg OS_NAME=$OS_NAME \
+             --build-arg https_proxy \
+             --build-arg http_proxy \
+             --build-arg no_proxy \
              -t $IMAGE \
              -f images/Dockerfile.${FLAVOR} ./
 
 docker run -v $PWD:/cOS \
            -v /var/run:/var/run \
+           -e https_proxy -e http_proxy -e no_proxy \
            -i --rm quay.io/costoolkit/elemental:v0.0.15-8a78e6b --name $ISO --debug build-iso --date=false --local --overlay-iso /cOS/overlay/files-iso $IMAGE --output /cOS/
 
 # See: https://github.com/rancher/elemental-cli/issues/228
