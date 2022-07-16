986a17f06f74e289064b7d22e7ff02c913980a83
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-07-16 14:29:11 +0000

    :robot: Add bundle test

---
+++ internal/machine/bundle_test.go
@@ -0,0 +1,34 @@
+package machine_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	. "github.com/c3os-io/c3os/internal/machine"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Bundle", func() {
+	Context("install", func() {
+		PIt("installs packages from luet repos", func() {
+			dir, err := ioutil.TempDir("", "test")
+			Expect(err).ToNot(HaveOccurred())
+			defer os.RemoveAll(dir)
+			os.MkdirAll(filepath.Join(dir, "var", "tmp", "luet"), os.ModePerm)
+			err = RunBundles([]BundleOption{WithDBPath(dir), WithRootFS(dir), WithTarget("package://utils/edgevpn")})
+			Expect(err).ToNot(HaveOccurred())
+			Expect(filepath.Join(dir, "usr", "bin", "edgevpn")).To(BeARegularFile())
+		})
+
+		It("installs from container images", func() {
+			dir, err := ioutil.TempDir("", "test")
+			Expect(err).ToNot(HaveOccurred())
+			defer os.RemoveAll(dir)
+			err = RunBundles([]BundleOption{WithDBPath(dir), WithRootFS(dir), WithTarget("container://quay.io/mocaccino/extra:edgevpn-utils-0.15.0")})
+			Expect(err).ToNot(HaveOccurred())
+			Expect(filepath.Join(dir, "usr", "bin", "edgevpn")).To(BeARegularFile())
+		})
+	})
+})
