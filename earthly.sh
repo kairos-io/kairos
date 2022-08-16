710a4df36e0844b242bc36afb1bc1953e066308f
Dan Ramich <dramich@users.noreply.github.com>
2022-08-16 15:07:51 -0600

    Update build command and docker.socket (#86)

--- earthly.sh
+++ earthly.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --privileged -v /var/run:/var/run --rm -t -v $(pwd):/workspace -v earthly-tmp:/tmp/earthly:rw earthly/earthly:v0.6.21 --allow-privileged $@
+docker run --privileged -v /var/run/docker.sock:/var/run/docker.sock --rm -t -v $(pwd):/workspace -v earthly-tmp:/tmp/earthly:rw earthly/earthly:v0.6.21 --allow-privileged $@
