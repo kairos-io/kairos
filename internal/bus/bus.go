209db95ee9776cf7a0f8a3f857f7bae6f4bc2a66
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-08-20 21:48:54 +0000

    :art: Make Initialize more idiomatic

--- internal/bus/bus.go
+++ internal/bus/bus.go
@@ -36,32 +36,34 @@ func (b *Bus) LoadProviders() {
 }
 
 func (b *Bus) Initialize() {
-	if !b.registered {
-		b.LoadProviders()
-		for i := range b.Manager.Events {
-			e := b.Manager.Events[i]
-			b.Manager.Response(e, func(p *pluggable.Plugin, r *pluggable.EventResponse) {
-				if os.Getenv("BUS_DEBUG") == "true" {
-					fmt.Println(
-						fmt.Sprintf("[provider event: %s]", e),
-						"received from",
-						p.Name,
-						"at",
-						p.Executable,
-						r,
-					)
-				}
-				if r.Errored() {
-					err := fmt.Sprintf("Provider %s at %s had an error: %s", p.Name, p.Executable, r.Error)
-					fmt.Println(err)
-					os.Exit(1)
-				} else {
-					if r.State != "" {
-						fmt.Println(fmt.Sprintf("[provider event: %s]", e), r.State)
-					}
-				}
-			})
-		}
-		b.registered = true
+	if b.registered {
+		return
 	}
+
+	b.LoadProviders()
+	for i := range b.Manager.Events {
+		e := b.Manager.Events[i]
+		b.Manager.Response(e, func(p *pluggable.Plugin, r *pluggable.EventResponse) {
+			if os.Getenv("BUS_DEBUG") == "true" {
+				fmt.Println(
+					fmt.Sprintf("[provider event: %s]", e),
+					"received from",
+					p.Name,
+					"at",
+					p.Executable,
+					r,
+				)
+			}
+			if r.Errored() {
+				err := fmt.Sprintf("Provider %s at %s had an error: %s", p.Name, p.Executable, r.Error)
+				fmt.Println(err)
+				os.Exit(1)
+			} else {
+				if r.State != "" {
+					fmt.Println(fmt.Sprintf("[provider event: %s]", e), r.State)
+				}
+			}
+		})
+	}
+	b.registered = true
 }
