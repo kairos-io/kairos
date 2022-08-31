486ccaea13ff00b8e09916b0dfe2da316dca80d7
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-08-31 10:22:12 +0000

    :robot: Auto bump c3os repos, consume arm64

--- .github/bump_cos.sh
+++ .github/bump_cos.sh
@@ -33,3 +33,17 @@ latest_tag_arm64=$(last_snapshot quay.io/costoolkit/releases-green-arm64)
 
 $YQ eval '.repositories[0].reference = "'$latest_tag'-repository.yaml"' -i repositories/repositories.yaml.tumbleweed
 $YQ eval '.repositories[1].reference = "'$latest_tag_arm64'-repository.yaml"' -i repositories/repositories.yaml.tumbleweed
+
+last_commit_snapshot() {
+    echo $(docker run --rm quay.io/skopeo/stable list-tags docker://$1 | jq -rc '.Tags | map(select( (. | contains("-repository.yaml")) )) | sort_by(. | sub("v";"") | sub("-repository.yaml";"") | sub("-";"") | split(".") | map(tonumber) ) | .[-1]' | sed "s/-repository.yaml//g")
+}
+
+latest_tag=$(last_commit_snapshot quay.io/c3os/packages)
+latest_tag_arm64=$(last_commit_snapshot quay.io/c3os/packages-arm64)
+
+for REPOFILE in repositories.yaml.tumbleweed repositories.yaml.ubuntu repositories.yaml.fedora repositories.yaml
+do
+    $YQ eval '.repositories[3].reference = "'$latest_tag'-repository.yaml"' -i repositories/$REPOFILE
+    $YQ eval '.repositories[4].reference = "'$latest_tag_arm64'-repository.yaml"' -i repositories/$REPOFILE
+done
+
