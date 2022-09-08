07b11cf27944dd97eeedf07770f2f78fd3a03647
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-09-08 21:50:06 +0000

    :robot: Evaluate anchors when bumping repositories
    the selector was resolving anchors and as such updating the key/value to which pointed to.
    This allows automatic bumps to flow again.

--- .github/bump_repositories.sh
+++ .github/bump_repositories.sh
@@ -7,32 +7,41 @@ last_snapshot() {
     echo $(docker run --rm quay.io/skopeo/stable list-tags docker://$1 | jq -rc '.Tags | map(select( (. | contains("-repository.yaml")) and ( . | contains("v")))) | sort_by(. | sub("v";"") | sub("-repository.yaml";"") | sub("-";"") | split(".") | map(tonumber) ) | .[-1]' | sed "s/-repository.yaml//g")
 }
 
+reference() {
+    nr=$1
+    tag=$2
+
+    echo ".repositories[$nr] |= . * { \"reference\": \"$tag-repository.yaml\" }"
+}
+
 YQ=${YQ:-docker run --rm -v "${PWD}":/workdir mikefarah/yq}
 set -x
 
 latest_tag=$(last_snapshot quay.io/costoolkit/releases-teal)
 latest_tag_arm64=$(last_snapshot quay.io/costoolkit/releases-teal-arm64)
 
-$YQ eval '.repositories[0].reference = "'$latest_tag'-repository.yaml"' -i repositories/repositories.yaml
-$YQ eval '.repositories[1].reference = "'$latest_tag_arm64'-repository.yaml"' -i repositories/repositories.yaml
+$YQ eval "$(reference 0 $latest_tag)" -i repositories/repositories.yaml
+$YQ eval "$(reference 1 $latest_tag_arm64)" -i repositories/repositories.yaml
 
 latest_tag_blue=$(last_snapshot quay.io/costoolkit/releases-blue)
 latest_tag_blue_arm64=$(last_snapshot quay.io/costoolkit/releases-blue-arm64)
 
-$YQ eval '.repositories[0].reference = "'$latest_tag_blue'-repository.yaml"' -i repositories/repositories.yaml.fedora
-$YQ eval '.repositories[1].reference = "'$latest_tag_blue_arm64'-repository.yaml"' -i repositories/repositories.yaml.fedora
+$YQ eval "$(reference 0 $latest_tag_blue)" -i repositories/repositories.yaml.fedora
+$YQ eval "$(reference 1 $latest_tag_blue_arm64)" -i repositories/repositories.yaml.fedora
+$YQ eval "$(reference 0 $latest_tag_blue)" -i repositories/repositories.yaml.rockylinux
+$YQ eval "$(reference 1 $latest_tag_blue_arm64)" -i repositories/repositories.yaml.rockylinux
 
 latest_tag_orange=$(last_snapshot quay.io/costoolkit/releases-orange)
 latest_tag_orange_arm64=$(last_snapshot quay.io/costoolkit/releases-orange-arm64)
 
-$YQ eval '.repositories[0].reference = "'$latest_tag_orange'-repository.yaml"' -i repositories/repositories.yaml.ubuntu
-$YQ eval '.repositories[1].reference = "'$latest_tag_orange_arm64'-repository.yaml"' -i repositories/repositories.yaml.ubuntu
+$YQ eval "$(reference 0 $latest_tag_orange)" -i repositories/repositories.yaml.ubuntu
+$YQ eval "$(reference 1 $latest_tag_orange_arm64)" -i repositories/repositories.yaml.ubuntu
 
 latest_tag=$(last_snapshot quay.io/costoolkit/releases-green)
 latest_tag_arm64=$(last_snapshot quay.io/costoolkit/releases-green-arm64)
 
-$YQ eval '.repositories[0].reference = "'$latest_tag'-repository.yaml"' -i repositories/repositories.yaml.tumbleweed
-$YQ eval '.repositories[1].reference = "'$latest_tag_arm64'-repository.yaml"' -i repositories/repositories.yaml.tumbleweed
+$YQ eval "$(reference 0 $latest_tag)" -i repositories/repositories.yaml.tumbleweed
+$YQ eval "$(reference 1 $latest_tag_arm64)" -i repositories/repositories.yaml.tumbleweed
 
 last_commit_snapshot() {
     echo $(docker run --rm quay.io/skopeo/stable list-tags docker://$1 | jq -rc '.Tags | map(select( (. | contains("-repository.yaml")) )) | sort_by(. | sub("v";"") | sub("-repository.yaml";"") | sub("-";"") | split(".") | map(tonumber) ) | .[-1]' | sed "s/-repository.yaml//g")
@@ -41,9 +50,9 @@ last_commit_snapshot() {
 latest_tag=$(last_commit_snapshot quay.io/c3os/packages)
 latest_tag_arm64=$(last_commit_snapshot quay.io/c3os/packages-arm64)
 
-for REPOFILE in repositories.yaml.tumbleweed repositories.yaml.ubuntu repositories.yaml.fedora repositories.yaml
+for REPOFILE in repositories.yaml.tumbleweed repositories.yaml.rockylinux repositories.yaml.ubuntu repositories.yaml.fedora repositories.yaml
 do
-    $YQ eval '.repositories[2].reference = "'$latest_tag'-repository.yaml"' -i repositories/$REPOFILE
-    $YQ eval '.repositories[3].reference = "'$latest_tag_arm64'-repository.yaml"' -i repositories/$REPOFILE
+    $YQ eval "$(reference 2 $latest_tag)" -i repositories/$REPOFILE
+    $YQ eval "$(reference 3 $latest_tag_arm64)" -i repositories/$REPOFILE
 done
 
