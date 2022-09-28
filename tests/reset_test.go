0d698eb0c21d370f1f8c4ca1b1db9b2fedfce95f
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-09-28 23:52:30 +0200

    :robot: Cleanup and refactor tests (#157)

--- tests/reset_test.go
+++ tests/reset_test.go
@@ -6,9 +6,9 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/kairos-io/kairos/tests/machine"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	. "github.com/spectrocloud/peg/matcher"
 )
 
 var _ = Describe("kairos reset test", Label("reset-test"), func() {
@@ -17,42 +17,36 @@ var _ = Describe("kairos reset test", Label("reset-test"), func() {
 			Fail("CLOUD_INIT must be set and must be pointing to a file as an absolute path")
 		}
 
-		machine.EventuallyConnects()
-	})
-
-	AfterEach(func() {
-		if CurrentGinkgoTestDescription().Failed {
-			gatherLogs()
-		}
+		EventuallyConnects()
 	})
 
 	Context("live cd", func() {
 		It("has default service active", func() {
 			if os.Getenv("FLAVOR") == "alpine" {
-				out, _ := machine.SSHCommand("sudo rc-status")
+				out, _ := Machine.Command("sudo rc-status")
 				Expect(out).Should(ContainSubstring("kairos"))
 				Expect(out).Should(ContainSubstring("kairos-agent"))
 				fmt.Println(out)
 			} else {
 				// Eventually(func() string {
-				// 	out, _ := machine.SSHCommand("sudo systemctl status kairosososososos-agent")
+				// 	out, _ := Machine.Command("sudo systemctl status kairosososososos-agent")
 				// 	return out
 				// }, 30*time.Second, 10*time.Second).Should(ContainSubstring("no network token"))
 
-				out, _ := machine.SSHCommand("sudo systemctl status kairos")
+				out, _ := Machine.Command("sudo systemctl status kairos")
 				Expect(out).Should(ContainSubstring("loaded (/etc/systemd/system/kairos.service; enabled;"))
 				fmt.Println(out)
 			}
 
-			out, _ := machine.SSHCommand("ls -liah /oem")
+			out, _ := Machine.Command("ls -liah /oem")
 			fmt.Println(out)
 			//	Expect(out).To(ContainSubstring("userdata.yaml"))
-			out, _ = machine.SSHCommand("cat /oem/userdata")
+			out, _ = Machine.Command("cat /oem/userdata")
 			fmt.Println(out)
-			out, _ = machine.SSHCommand("sudo ps aux")
+			out, _ = Machine.Command("sudo ps aux")
 			fmt.Println(out)
 
-			out, _ = machine.SSHCommand("sudo lsblk")
+			out, _ = Machine.Command("sudo lsblk")
 			fmt.Println(out)
 
 		})
@@ -61,7 +55,7 @@ var _ = Describe("kairos reset test", Label("reset-test"), func() {
 	Context("auto installs", func() {
 		It("to disk with custom config", func() {
 			Eventually(func() string {
-				out, _ := machine.SSHCommand("sudo ps aux")
+				out, _ := Machine.Command("sudo ps aux")
 				return out
 			}, 30*time.Minute, 1*time.Second).Should(
 				Or(
@@ -73,7 +67,7 @@ var _ = Describe("kairos reset test", Label("reset-test"), func() {
 	Context("reboots and passes functional tests", func() {
 		It("has grubenv file", func() {
 			Eventually(func() string {
-				out, _ := machine.SSHCommand("sudo cat /oem/grubenv")
+				out, _ := Machine.Command("sudo cat /oem/grubenv")
 				return out
 			}, 40*time.Minute, 1*time.Second).Should(
 				Or(
@@ -82,29 +76,29 @@ var _ = Describe("kairos reset test", Label("reset-test"), func() {
 		})
 
 		It("resets", func() {
-			_, err := machine.Sudo("echo 'test' > /usr/local/test")
+			_, err := Sudo("echo 'test' > /usr/local/test")
 			Expect(err).ToNot(HaveOccurred())
 
-			_, err = machine.Sudo("echo 'testoem' > /oem/test")
+			_, err = Sudo("echo 'testoem' > /oem/test")
 			Expect(err).ToNot(HaveOccurred())
 
-			machine.HasFile("/oem/test")
-			machine.HasFile("/usr/local/test")
+			HasFile("/oem/test")
+			HasFile("/usr/local/test")
 
-			_, err = machine.Sudo("grub2-editenv /oem/grubenv set next_entry=statereset")
+			_, err = Sudo("grub2-editenv /oem/grubenv set next_entry=statereset")
 			Expect(err).ToNot(HaveOccurred())
 
-			machine.Reboot()
+			Reboot()
 
 			Eventually(func() string {
-				out, _ := machine.Sudo("if [ -f /usr/local/test ]; then echo ok; else echo wrong; fi")
+				out, _ := Sudo("if [ -f /usr/local/test ]; then echo ok; else echo wrong; fi")
 				return out
 			}, 40*time.Minute, 1*time.Second).Should(
 				Or(
 					ContainSubstring("wrong"),
 				))
 			Eventually(func() string {
-				out, _ := machine.Sudo("if [ -f /oem/test ]; then echo ok; else echo wrong; fi")
+				out, _ := Sudo("if [ -f /oem/test ]; then echo ok; else echo wrong; fi")
 				return out
 			}, 40*time.Minute, 1*time.Second).Should(
 				Or(
