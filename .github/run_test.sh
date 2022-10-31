792bc3910a1b339622877864caf32c921572c795
Ettore Di Giacinto <mudler@users.noreply.github.com>
2022-10-31 11:56:55 +0100

    :robot: Fix ginkgo deps in install_test (#341)
    Signed-off-by: Ettore Di Giacinto <mudler@users.noreply.github.com>
    
    Signed-off-by: Ettore Di Giacinto <mudler@users.noreply.github.com>

--- .github/run_test.sh
+++ .github/run_test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-go install github.com/onsi/ginkgo/v2/ginkgo
+go get github.com/onsi/gomega/...
+go get github.com/onsi/ginkgo/v2/ginkgo/internal@v2.1.4
+go get github.com/onsi/ginkgo/v2/ginkgo/generators@v2.1.4
+go get github.com/onsi/ginkgo/v2/ginkgo/labels@v2.1.4
+go install -mod=mod github.com/onsi/ginkgo/v2/ginkgo
 ginkgo --label-filter "$1" --fail-fast -r ./tests/
