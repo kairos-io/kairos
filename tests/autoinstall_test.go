kairos-io/kairos

commit f5233429662156b58fd4c190c77ddc04411dad5f

:seedling: Glob all configs found (#502)
* :seedling: Glob all configs found

This allows to read all the configs found during scanning - this is especially useful in first boot as it allows
to have separate config file logic split into several files

Signed-off-by: Ettore Di Giacinto <mudler@users.noreply.github.com>

* :seedling: Simplify config merge logic

Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

* WIP

Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

* :seedling: Make the function work

Signed-off-by: Ettore Di Giacinto <mudler@users.noreply.github.com>
Signed-off-by: mudler <mudler@kairos.io>

Signed-off-by: Ettore Di Giacinto <mudler@users.noreply.github.com>
Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>
Signed-off-by: mudler <mudler@kairos.io>
Co-authored-by: Dimitris Karakasilis <dimitris@karakasilis.me>

pkg/config/config.go
@@ -12,6 +12,7 @@ import (
 	retry "github.com/avast/retry-go"
 	"github.com/kairos-io/kairos/pkg/machine"
 	"github.com/kairos-io/kairos/sdk/bundles"
+	"github.com/kairos-io/kairos/sdk/unstructured"
 	yip "github.com/mudler/yip/pkg/schema"
 
 	"gopkg.in/yaml.v2"
@@ -33,7 +34,6 @@ type Config struct {
 	Install *Install `yaml:"install,omitempty"`
 	//cloudFileContent string
 	originalData       map[string]interface{}
-	location           string
 	header             string
 	ConfigURL          string            `yaml:"config_url,omitempty"`
 	Options            map[string]string `yaml:"options,omitempty"`
@@ -87,10 +87,6 @@ func (c Config) Unmarshal(o interface{}) error {
 	return yaml.Unmarshal([]byte(c.String()), o)
 }
 
-func (c Config) Location() string {
-	return c.location
-}
-
 func (c Config) Data() map[string]interface{} {
 	return c.originalData
 }
@@ -110,69 +106,23 @@ func (c Config) String() string {
 	return string(dat)
 }
 
-func (c Config) IsValid() bool {
-	return c.Install != nil ||
-		c.ConfigURL != "" ||
-		len(c.Bundles) != 0
-}
-
-func Scan(opts ...Option) (c *Config, err error) {
-
-	o := &Options{}
-
-	if err := o.Apply(opts...); err != nil {
-		return nil, err
-	}
-
-	dir := o.ScanDir
-
-	c = &Config{}
+func allFiles(dir []string) []string {
 	files := []string{}
 	for _, d := range dir {
 		if f, err := listFiles(d); err == nil {
 			files = append(files, f...)
 		}
 	}
+	return files
+}
 
-	configFound := false
-	lastYamlFileFound := ""
-
-	// Scanning happens as best-effort, therefore unmarshalling skips errors here.
-	for _, f := range files {
-		if fileSize(f) > 1.0 {
-			//fmt.Println("warning: Skipping file ", f, "as exceeds 1 MB in size")
-			continue
-		}
-		b, err := os.ReadFile(f)
-		if err == nil {
-			// best effort. skip lint checks
-			yaml.Unmarshal(b, c) //nolint:errcheck
-			if exists, header := HasHeader(string(b), ""); c.IsValid() || exists {
-				c.location = f
-				yaml.Unmarshal(b, &c.originalData) //nolint:errcheck
-				configFound = true
-				if exists {
-					c.header = header
-				}
-				break
-			}
-
-			// record back the only yaml file found (if any)
-			if strings.HasSuffix(strings.ToLower(f), "yaml") || strings.HasSuffix(strings.ToLower(f), "yml") {
-				lastYamlFileFound = f
-			}
-		}
+func Scan(opts ...Option) (c *Config, err error) {
+	o := &Options{}
+	if err := o.Apply(opts...); err != nil {
+		return nil, err
 	}
 
-	// use last recorded if no config is found valid
-	if !configFound && lastYamlFileFound != "" {
-		b, err := os.ReadFile(lastYamlFileFound)
-		if err == nil {
-			yaml.Unmarshal(b, c) //nolint:errcheck
-			c.location = lastYamlFileFound
-			yaml.Unmarshal(b, &c.originalData) //nolint:errcheck
-		}
-	}
+	c = parseConfig(o.ScanDir)
 
 	if o.MergeBootCMDLine {
 		d, err := machine.DotToYAML(o.BootCMDLineFile)
@@ -253,7 +203,9 @@ func listFiles(dir string) ([]string, error) {
 			if err != nil {
 				return nil
 			}
-			content = append(content, path)
+			if !info.IsDir() {
+				content = append(content, path)
+			}
 
 			return nil
 		})
@@ -306,4 +258,58 @@ func MergeYAML(objs ...interface{}) ([]byte, error) {
 
 func AddHeader(header, data string) string {
 	return fmt.Sprintf("%s\n%s", header, data)
+}
+
+func FindYAMLWithKey(s string, opts ...Option) ([]string, error) {
+	o := &Options{}
+
+	result := []string{}
+	if err := o.Apply(opts...); err != nil {
+		return result, err
+	}
+
+	files := allFiles(o.ScanDir)
+
+	for _, f := range files {
+		dat, err := os.ReadFile(f)
+		if err != nil {
+			fmt.Printf("warning: skipping file '%s' - %s\n", f, err.Error())
+		}
+
+		found, err := unstructured.YAMLHasKey(s, dat)
+		if err != nil {
+			fmt.Printf("warning: skipping file '%s' - %s\n", f, err.Error())
+		}
+
+		if found {
+			result = append(result, f)
+		}
+
+	}
+
+	return result, nil
+}
+
+// parseConfig merges all config back in one structure.
+func parseConfig(dir []string) *Config {
+	files := allFiles(dir)
+	c := &Config{}
+	for _, f := range files {
+		if fileSize(f) > 1.0 {
+			fmt.Printf("warning: skipping %s. too big (>1MB)\n", f)
+			continue
+		}
+		b, err := os.ReadFile(f)
+		if err != nil {
+			fmt.Printf("warning: skipping %s. %s\n", f, err.Error())
+			continue
+		}
+		yaml.Unmarshal(b, c)               //nolint:errcheck
+		yaml.Unmarshal(b, &c.originalData) //nolint:errcheck
+		if exists, header := HasHeader(string(b), ""); exists {
+			c.header = header
+		}
+	}
+
+	return c
 }

pkg/config/config_test.go
@@ -22,6 +22,7 @@ import (
 	. "github.com/kairos-io/kairos/pkg/config"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"gopkg.in/yaml.v3"
 )
 
 type TConfig struct {
@@ -30,20 +31,19 @@ type TConfig struct {
 	} `yaml:"kairos"`
 }
 
-var _ = Describe("Get config", func() {
+var _ = Describe("Config", func() {
+	var d string
+	BeforeEach(func() {
+		d, _ = os.MkdirTemp("", "xxxx")
+	})
+
+	AfterEach(func() {
+		if d != "" {
+			os.RemoveAll(d)
+		}
+	})
+
 	Context("directory", func() {
-
-		var d string
-		BeforeEach(func() {
-			d, _ = os.MkdirTemp("", "xxxx")
-		})
-
-		AfterEach(func() {
-			if d != "" {
-				os.RemoveAll(d)
-			}
-		})
-
 		headerCheck := func(c *Config) {
 			ok, header := HasHeader(c.String(), DefaultHeader)
 			ExpectWithOffset(1, ok).To(BeTrue())
@@ -58,6 +58,37 @@ var _ = Describe("Get config", func() {
 			Expect(err).ToNot(HaveOccurred())
 			headerCheck(c)
 			Expect(c.Options["foo"]).To(Equal("bar"))
+		})
+
+		It("reads multiple config files", func() {
+			var cc string = `#kairos-config
+baz: bar
+kairos:
+  network_token: foo
+`
+			var c2 string = `
+b: f
+c: d
+`
+
+			err := os.WriteFile(filepath.Join(d, "test"), []byte(cc), os.ModePerm)
+			Expect(err).ToNot(HaveOccurred())
+
+			err = os.WriteFile(filepath.Join(d, "b"), []byte(c2), os.ModePerm)
+			Expect(err).ToNot(HaveOccurred())
+
+			c, err := Scan(Directories(d))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(c).ToNot(BeNil())
+			providerCfg := &TConfig{}
+			err = c.Unmarshal(providerCfg)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(providerCfg.Kairos).ToNot(BeNil())
+			Expect(providerCfg.Kairos.NetworkToken).To(Equal("foo"))
+			all := map[string]string{}
+			yaml.Unmarshal([]byte(c.String()), &all)
+			Expect(all["b"]).To(Equal("f"))
+			Expect(all["baz"]).To(Equal("bar"))
 		})
 
 		It("reads config file greedly", func() {
@@ -148,6 +179,56 @@ config_url: "https://gist.githubusercontent.com/mudler/7e3d0426fce8bfaaeb2644f83
 			Expect(c.String()).ToNot(Equal(cc))
 
 			headerCheck(c)
+		})
+	})
+
+	Describe("FindYAMLWithKey", func() {
+		var c1Path, c2Path string
+
+		BeforeEach(func() {
+			var c1 = `
+a: 1
+b:
+  c: foo
+d:
+  e: bar
+`
+
+			var c2 = `
+b:
+  c: foo2
+`
+			c1Path = filepath.Join(d, "c1.yaml")
+			c2Path = filepath.Join(d, "c2.yaml")
+
+			err := os.WriteFile(c1Path, []byte(c1), os.ModePerm)
+			Expect(err).ToNot(HaveOccurred())
+			err = os.WriteFile(c2Path, []byte(c2), os.ModePerm)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("can find a top level key", func() {
+			r, err := FindYAMLWithKey("a", Directories(d))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(r).To(Equal([]string{c1Path}))
+		})
+
+		It("can find a nested key", func() {
+			r, err := FindYAMLWithKey("d.e", Directories(d))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(r).To(Equal([]string{c1Path}))
+		})
+
+		It("returns multiple files when key exists in them", func() {
+			r, err := FindYAMLWithKey("b.c", Directories(d))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(r).To(ContainElements(c1Path, c2Path))
+		})
+
+		It("return an empty list when key is not found", func() {
+			r, err := FindYAMLWithKey("does.not.exist", Directories(d))
+			Expect(err).ToNot(HaveOccurred())
+			Expect(r).To(BeEmpty())
 		})
 	})
 })

sdk/unstructured/yaml.go
@@ -9,6 +9,35 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+func YAMLHasKey(query string, content []byte) (bool, error) {
+	data := map[string]interface{}{}
+
+	err := yaml.Unmarshal([]byte(content), &data)
+	if err != nil {
+		return false, err
+	}
+
+	c, err := gojq.Parse(fmt.Sprintf(".%s | ..", query))
+	if err != nil {
+		return false, err
+	}
+	code, err := gojq.Compile(c)
+	if err != nil {
+		return false, err
+	}
+	iter := code.Run(data)
+
+	v, _ := iter.Next()
+
+	if err, ok := v.(error); ok {
+		return false, err
+	}
+	if v == nil {
+		return false, fmt.Errorf("not found")
+	}
+	return true, nil
+}
+
 func jq(command string, data map[string]interface{}) (map[string]interface{}, error) {
 	query, err := gojq.Parse(command)
 	if err != nil {
@@ -22,7 +51,7 @@ func jq(command string, data map[string]interface{}) (map[string]interface{}, er
 
 	v, ok := iter.Next()
 	if !ok {
-		return nil, errors.New("failed getting rsult from gojq")
+		return nil, errors.New("failed getting result from gojq")
 	}
 	if err, ok := v.(error); ok {
 		return nil, err

commit b8d3373376b4d67d7535d6823f23f0b6d7efdeb1

:bug: Process only cloud configs
Regression introduced in https://github.com/kairos-io/kairos/pull/502.
Now also files like .lst inside the initramfs are globbed inside /oem/90_custom.yaml

Signed-off-by: Ettore Di Giacinto <mudler@users.noreply.github.com>

pkg/config/config.go
@@ -299,15 +299,19 @@ func parseConfig(dir []string) *Config {
 			fmt.Printf("warning: skipping %s. too big (>1MB)\n", f)
 			continue
 		}
-		b, err := os.ReadFile(f)
-		if err != nil {
-			fmt.Printf("warning: skipping %s. %s\n", f, err.Error())
-			continue
-		}
-		yaml.Unmarshal(b, c)               //nolint:errcheck
-		yaml.Unmarshal(b, &c.originalData) //nolint:errcheck
-		if exists, header := HasHeader(string(b), ""); exists {
-			c.header = header
+		if strings.Contains(f, "userdata") || filepath.Ext(f) == ".yml" || filepath.Ext(f) == ".yaml" {
+			b, err := os.ReadFile(f)
+			if err != nil {
+				fmt.Printf("warning: skipping %s. %s\n", f, err.Error())
+				continue
+			}
+			yaml.Unmarshal(b, c)               //nolint:errcheck
+			yaml.Unmarshal(b, &c.originalData) //nolint:errcheck
+			if exists, header := HasHeader(string(b), ""); exists {
+				c.header = header
+			}
+		} else {
+			fmt.Printf("warning: skipping %s (extension).\n", f)
 		}
 	}
 

pkg/config/config_test.go
@@ -71,10 +71,10 @@ b: f
 c: d
 `
 
-			err := os.WriteFile(filepath.Join(d, "test"), []byte(cc), os.ModePerm)
+			err := os.WriteFile(filepath.Join(d, "test.yaml"), []byte(cc), os.ModePerm)
 			Expect(err).ToNot(HaveOccurred())
 
-			err = os.WriteFile(filepath.Join(d, "b"), []byte(c2), os.ModePerm)
+			err = os.WriteFile(filepath.Join(d, "b.yaml"), []byte(c2), os.ModePerm)
 			Expect(err).ToNot(HaveOccurred())
 
 			c, err := Scan(Directories(d))
@@ -99,9 +99,9 @@ kairos:
   network_token: foo
 `
 
-			err := os.WriteFile(filepath.Join(d, "test"), []byte(cc), os.ModePerm)
+			err := os.WriteFile(filepath.Join(d, "test.yaml"), []byte(cc), os.ModePerm)
 			Expect(err).ToNot(HaveOccurred())
-			err = os.WriteFile(filepath.Join(d, "b"), []byte(`
+			err = os.WriteFile(filepath.Join(d, "b.yaml"), []byte(`
 fooz:
 			`), os.ModePerm)
 			Expect(err).ToNot(HaveOccurred())
@@ -127,7 +127,7 @@ bb:
   nothing: "foo"
 `
 
-			err := os.WriteFile(filepath.Join(d, "test"), []byte(cc), os.ModePerm)
+			err := os.WriteFile(filepath.Join(d, "test.yaml"), []byte(cc), os.ModePerm)
 			Expect(err).ToNot(HaveOccurred())
 			err = os.WriteFile(filepath.Join(d, "b"), []byte(`zz.foo="baa" options.foo=bar`), os.ModePerm)
 			Expect(err).ToNot(HaveOccurred())
@@ -151,7 +151,7 @@ bb:
 config_url: "https://gist.githubusercontent.com/mudler/ab26e8dd65c69c32ab292685741ca09c/raw/bafae390eae4e6382fb1b68293568696823b3103/test.yaml"
 `
 
-			err := os.WriteFile(filepath.Join(d, "test"), []byte(cc), os.ModePerm)
+			err := os.WriteFile(filepath.Join(d, "test.yaml"), []byte(cc), os.ModePerm)
 			Expect(err).ToNot(HaveOccurred())
 
 			c, err := Scan(Directories(d))
@@ -168,7 +168,7 @@ config_url: "https://gist.githubusercontent.com/mudler/ab26e8dd65c69c32ab2926857
 config_url: "https://gist.githubusercontent.com/mudler/7e3d0426fce8bfaaeb2644f83a9bfe0c/raw/77ded58aab3ee2a8d4117db95e078f81fd08dfde/testgist.yaml"
 `
 
-			err := os.WriteFile(filepath.Join(d, "test"), []byte(cc), os.ModePerm)
+			err := os.WriteFile(filepath.Join(d, "test.yaml"), []byte(cc), os.ModePerm)
 			Expect(err).ToNot(HaveOccurred())
 
 			c, err := Scan(Directories(d))

tests/autoinstall_test.go
@@ -163,6 +163,13 @@ var _ = Describe("kairos autoinstall test", Label("autoinstall-test"), func() {
 			Expect(out).To(ContainSubstring("shared"))
 		})
 
+		It("doesn't has grub data into the cloud config", func() {
+			out, err := Sudo(`cat /oem/90_custom.yaml`)
+			Expect(err).ToNot(HaveOccurred(), out)
+			Expect(out).ToNot(ContainSubstring("vga_text"))
+			Expect(out).ToNot(ContainSubstring("videotest"))
+		})
+
 		It("has corresponding state", func() {
 			out, err := Sudo("kairos-agent state")
 			Expect(err).ToNot(HaveOccurred())