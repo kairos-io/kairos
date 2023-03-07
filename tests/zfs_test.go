ac65367ef1c364696c1edea5c083d7338d585fe9
Dimitris Karakasilis <dimitris@karakasilis.me>
2023-03-07 18:50:06 +0200

    Migrate to peg's new "VM" construct
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- tests/zfs_test.go
+++ tests/zfs_test.go
@@ -7,40 +7,48 @@ import (
 )
 
 var _ = Describe("kairos zfs test", Label("zfs"), func() {
+
+	var vm VM
+
 	BeforeEach(func() {
-		EventuallyConnects(1200)
+		_, vm = startVM()
+		vm.EventuallyConnects(1200)
 	})
 
-	Context("install", func() {
-		It("to disk with zfs kernel module loaded", func() {
-			err := Machine.SendFile("assets/zfs.yaml", "/tmp/zfs.yaml", "0770")
+	AfterEach(func() {
+		Expect(vm.Destroy(nil)).ToNot(HaveOccurred())
+	})
+
+	It("works", func() {
+		err := vm.Scp("assets/zfs.yaml", "/tmp/zfs.yaml", "0770")
+		Expect(err).ToNot(HaveOccurred())
+
+		By("installing to disk with zfs kernel module loaded", func() {
+			out, err := vm.Sudo("kairos-agent manual-install --device auto /tmp/zfs.yaml")
+			Expect(err).ToNot(HaveOccurred(), out)
+			out, err = vm.Sudo("sync")
+			Expect(err).ToNot(HaveOccurred(), out)
+			err = vm.DetachCD()
 			Expect(err).ToNot(HaveOccurred())
-
-			out, err := Sudo("kairos-agent manual-install --device auto /tmp/zfs.yaml")
-			Expect(err).ToNot(HaveOccurred(), out)
-			out, err = Sudo("sync")
-			Expect(err).ToNot(HaveOccurred(), out)
-			detachAndReboot()
+			vm.Reboot()
 		})
-	})
 
-	Context("create pool", func() {
-		It("with two disks and mount it at /mnt/pool0", func() {
-			out, err := Sudo("dd if=/dev/zero of=/usr/local/disk0.img bs=1M count=1024")
+		By("creating a pool with two disks", func() {
+			out, err := vm.Sudo("dd if=/dev/zero of=/usr/local/disk0.img bs=1M count=1024")
 			Expect(err).ToNot(HaveOccurred(), out)
-			out, err = Sudo("dd if=/dev/zero of=/usr/local/disk1.img bs=1M count=1024")
+			out, err = vm.Sudo("dd if=/dev/zero of=/usr/local/disk1.img bs=1M count=1024")
 			Expect(err).ToNot(HaveOccurred(), out)
-			out, err = Sudo("zpool create pool0 -m /usr/local/pool0 /usr/local/disk0.img /usr/local/disk1.img")
+			out, err = vm.Sudo("zpool create pool0 -m /usr/local/pool0 /usr/local/disk0.img /usr/local/disk1.img")
 			Expect(err).ToNot(HaveOccurred(), out)
 		})
 
-		It("to be writable", func() {
-			out, err := Sudo("touch /usr/local/pool0/test")
+		By("checking if it's writable", func() {
+			out, err := vm.Sudo("touch /usr/local/pool0/test")
 			Expect(err).ToNot(HaveOccurred(), out)
 		})
 
-		It("file to persist", func() {
-			out, err := Sudo("ls /usr/local/pool0")
+		By("checking if a file persists", func() {
+			out, err := vm.Sudo("ls /usr/local/pool0")
 			Expect(err).ToNot(HaveOccurred(), out)
 			Expect(out).To(ContainSubstring("test"))
 		})
