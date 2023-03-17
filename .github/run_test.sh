590c55353606cc0fbe85a4dbef3dea6ce2187a85
Itxaka <itxaka.garcia@spectrocloud.com>
2023-03-17 14:04:43 +0100

    :arrow_up: Bump repositories (#1163)

--- .github/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-go get github.com/onsi/gomega/...
-go get github.com/onsi/ginkgo/v2/ginkgo/internal@v2.1.4
-go get github.com/onsi/ginkgo/v2/ginkgo/generators@v2.1.4
-go get github.com/onsi/ginkgo/v2/ginkgo/labels@v2.1.4
-go install -mod=mod github.com/onsi/ginkgo/v2/ginkgo
-ginkgo --label-filter "$1" --fail-fast -r ./tests/
