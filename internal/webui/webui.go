4d16878d3f84794ad2d86256eab93b0226286492
Mauro Morales <mauro.morales@spectrocloud.com>
2023-04-06 15:01:41 +0200

    :bug: Add yaml extension to temp file created by webui (#1260)
    Add yaml extension to temp file created by webui
    
    Signed-off-by: Mauro Morales <mauro.morales@spectrocloud.com>

--- internal/webui/webui.go
+++ internal/webui/webui.go
@@ -209,7 +209,7 @@ func Start(ctx context.Context) error {
 		args = append(args, "--device", installationDevice)
 
 		// create tempfile to store cloud-config, bail out if we fail as we couldn't go much further
-		file, err := os.CreateTemp("", "install-webui")
+		file, err := os.CreateTemp("", "install-webui-*.yaml")
 		if err != nil {
 			log.Fatalf("could not create tmpfile for cloud-config: %s", err.Error())
 		}
