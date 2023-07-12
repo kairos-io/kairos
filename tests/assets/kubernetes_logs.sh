a7b7d780af69ae5f76e148a8fb54901c41851ef2
Itxaka <itxaka.garcia@spectrocloud.com>
2023-07-12 22:39:36 +0200

    Update tests/assets/kubernetes_logs.sh
    Co-authored-by: Mauro Morales <contact@mauromorales.com>
    Signed-off-by: Itxaka <itxaka.garcia@spectrocloud.com>

--- tests/assets/kubernetes_logs.sh
+++ tests/assets/kubernetes_logs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for n in $(k3s kubectl  get namespace -A | tr -s ' ' | cut -f1 -d' ' | tail -n +2); do
+for n in $(k3s kubectl get namespace -A | tr -s ' ' | cut -f1 -d' ' | tail -n +2); do
     for p in $(k3s kubectl get pods -n "$n" | tr -s ' ' | cut -f1 -d' ' | tail -n +2); do
         echo ---------------------------
         echo "$n" - "$p"
