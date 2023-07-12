1b98143b4a1ed6b15429c44f504869460d62bb6d
Itxaka <itxaka.garcia@spectrocloud.com>
2023-07-12 22:43:11 +0200

    Build provider in kairos and bring provider-kairos tests (#1592)
    Co-authored-by: Mauro Morales <contact@mauromorales.com>

---
+++ tests/provider_install_qrcode_test.go
@@ -0,0 +1,142 @@
+// nolint
+package mos_test
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/lmittmann/ppm"
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/qrcode"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	. "github.com/spectrocloud/peg/matcher"
+)
+
+var _ = Describe("kairos qr code install", Label("provider", "provider-qrcode-install"), func() {
+	var vm VM
+
+	BeforeEach(func() {
+		_, vm = startVM()
+		vm.EventuallyConnects(1200)
+	})
+
+	AfterEach(func() {
+		if CurrentSpecReport().Failed() {
+			gatherLogs(vm)
+			serial, _ := os.ReadFile(filepath.Join(vm.StateDir, "serial.log"))
+			_ = os.MkdirAll("logs", os.ModePerm|os.ModeDir)
+			_ = os.WriteFile(filepath.Join("logs", "serial.log"), serial, os.ModePerm)
+			fmt.Println(string(serial))
+		}
+		vm.Destroy(nil)
+	})
+
+	It("installs to disk with custom config", func() {
+		By("checking if is has default service active")
+		if isFlavor(vm, "alpine") {
+			out, _ := vm.Sudo("rc-status")
+			Expect(out).Should(ContainSubstring("kairos"))
+			Expect(out).Should(ContainSubstring("kairos-agent"))
+		} else {
+			// Eventually(func() string {
+			// 	out, _ := machine.SSHCommand("sudo systemctl status kairos-agent")
+			// 	return out
+			// }, 30*time.Second, 10*time.Second).Should(ContainSubstring("no network token"))
+
+			out, _ := vm.Sudo("systemctl status kairos")
+			Expect(out).Should(ContainSubstring("loaded (/etc/systemd/system/kairos.service; enabled; vendor preset: disabled)"))
+		}
+
+		By("checking cmdline")
+		v, err := vm.Sudo("cat /proc/cmdline")
+		Expect(err).ToNot(HaveOccurred(), v)
+		Expect(v).To(ContainSubstring("rd.cos.disable"))
+
+		var fileName string
+		By("waiting until the qr code is shown")
+		Eventually(func() string {
+			fileName = getQRImage(vm)
+
+			return fileName
+		}, 10*time.Minute, 10*time.Second).ShouldNot(BeEmpty())
+
+		By("find the correct device (qemu vs vbox)")
+		device, err := vm.Sudo(`[[ -e /dev/sda ]] && echo "/dev/sda" || echo "/dev/vda"`)
+		Expect(err).ToNot(HaveOccurred(), device)
+
+		By("registering with a screenshot")
+		err = register("fatal", fileName, "./assets/config.yaml", strings.TrimSpace(device), true)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("waiting until it reboots to installed system")
+		Eventually(func() string {
+			v, _ := vm.Sudo("kairos-agent state get boot")
+			return strings.TrimSpace(v)
+		}, 30*time.Minute, 10*time.Second).Should(ContainSubstring("active_boot"))
+
+		Eventually(func() string {
+			v, _ := vm.Sudo("cat /proc/cmdline")
+			return v
+		}, 10*time.Minute, 10*time.Second).ShouldNot(ContainSubstring("rd.cos.disable"))
+	})
+})
+
+// getQRImage returns the path to a screenshot with a QR code or empty
+// if no QR code is found
+func getQRImage(vm VM) string {
+	var fileName string
+	image.RegisterFormat("ppm", "ppm", ppm.Decode, ppm.DecodeConfig)
+
+	var err error
+	fileName, err = vm.Screenshot()
+	if err != nil {
+		os.RemoveAll(fileName)
+	}
+	Expect(err).ToNot(HaveOccurred())
+
+	// open and decode image file
+	file, err := os.Open(fileName)
+	if err != nil {
+		os.RemoveAll(fileName)
+	}
+	img, _, err := image.Decode(file)
+	if err != nil {
+		os.RemoveAll(fileName)
+	}
+	Expect(err).ToNot(HaveOccurred())
+
+	// prepare BinaryBitmap
+	bmp, err := gozxing.NewBinaryBitmapFromImage(img)
+	if err != nil {
+		os.RemoveAll(fileName)
+	}
+	Expect(err).ToNot(HaveOccurred())
+
+	// decode image
+	qrReader := qrcode.NewQRCodeReader()
+	_, err = qrReader.Decode(bmp, nil)
+	if err != nil {
+		os.RemoveAll(fileName)
+
+		return ""
+	}
+
+	// Encode to png because go-nodepair doesn't understand `ppm`
+	// Relevant: https://github.com/mudler/go-nodepair/pull/1
+	buf := new(bytes.Buffer)
+	err = png.Encode(buf, img)
+	Expect(err).ToNot(HaveOccurred())
+
+	// Replace with png data
+	err = os.WriteFile(fileName, buf.Bytes(), os.ModePerm)
+	Expect(err).ToNot(HaveOccurred())
+
+	return fileName
+}
---
+++ tests/provider_upgrade_k8s_test.go
@@ -0,0 +1,150 @@
+// nolint
+package mos_test
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	. "github.com/spectrocloud/peg/matcher"
+)
+
+var _ = Describe("k3s upgrade test", Label("provider", "provider-upgrade-k8s"), func() {
+	var vm VM
+
+	BeforeEach(func() {
+		_, vm = startVM()
+		vm.EventuallyConnects(1200)
+	})
+
+	AfterEach(func() {
+		if CurrentGinkgoTestDescription().Failed {
+			gatherLogs(vm)
+		}
+		vm.Destroy(nil)
+	})
+
+	It("installs to disk with custom config", func() {
+		By("checking if it has default service active")
+		if isFlavor(vm, "alpine") {
+			out, _ := vm.Sudo("rc-status")
+			Expect(out).Should(ContainSubstring("kairos"))
+			Expect(out).Should(ContainSubstring("kairos-agent"))
+			out, _ = vm.Sudo("ps aux")
+			Expect(out).Should(ContainSubstring("/usr/sbin/crond"))
+		} else {
+			out, _ := vm.Sudo("systemctl status kairos")
+			Expect(out).Should(ContainSubstring("loaded (/etc/systemd/system/kairos.service; enabled; vendor preset: disabled)"))
+
+			out, _ = vm.Sudo("systemctl status logrotate.timer")
+			Expect(out).Should(ContainSubstring("active (waiting)"))
+		}
+
+		By("copy the config")
+		err := vm.Scp("assets/single.yaml", "/tmp/config.yaml", "0770")
+		Expect(err).ToNot(HaveOccurred())
+
+		By("find the correct device (qemu vs vbox)")
+		device, err := vm.Sudo(`[[ -e /dev/sda ]] && echo "/dev/sda" || echo "/dev/vda"`)
+		Expect(err).ToNot(HaveOccurred(), device)
+
+		By("installing")
+		cmd := fmt.Sprintf("kairos-agent manual-install --device %s /tmp/config.yaml", strings.TrimSpace(device))
+		out, err := vm.Sudo(cmd)
+		Expect(err).ToNot(HaveOccurred(), out)
+		Expect(out).Should(ContainSubstring("Running after-install hook"))
+
+		out, err = vm.Sudo("sync")
+		Expect(err).ToNot(HaveOccurred(), out)
+
+		By("rebooting after install")
+		vm.Reboot()
+
+		By("checking default services are on after first boot")
+		if isFlavor(vm, "alpine") {
+			Eventually(func() string {
+				out, _ := vm.Sudo("rc-status")
+				return out
+			}, 30*time.Second, 10*time.Second).Should(And(
+				ContainSubstring("kairos"),
+				ContainSubstring("kairos-agent")))
+		} else {
+			Eventually(func() string {
+				out, _ := vm.Sudo("systemctl status kairos-agent")
+				return out
+			}, 30*time.Second, 10*time.Second).Should(ContainSubstring(
+				"loaded (/etc/systemd/system/kairos-agent.service; enabled; vendor preset: disabled)"))
+
+			Eventually(func() string {
+				out, _ := vm.Sudo("systemctl status systemd-timesyncd")
+				return out
+			}, 30*time.Second, 10*time.Second).Should(ContainSubstring(
+				"loaded (/usr/lib/systemd/system/systemd-timesyncd.service; enabled; vendor preset: disabled)"))
+		}
+
+		By("checking if kairos-agent has started")
+		Eventually(func() string {
+			var out string
+			if isFlavor(vm, "alpine") {
+				out, _ = vm.Sudo("rc-service kairos-agent status")
+			} else {
+				out, _ = vm.Sudo("systemctl status kairos-agent")
+			}
+			return out
+		}, 900*time.Second, 10*time.Second).Should(Or(ContainSubstring("One time bootstrap starting"), ContainSubstring("status: started")))
+		By("Checking agent provider correct start")
+		Eventually(func() string {
+			out, _ := vm.Sudo("cat /var/log/kairos/agent-provider.log")
+			return out
+		}, 900*time.Second, 10*time.Second).Should(Or(ContainSubstring("One time bootstrap starting"), ContainSubstring("Sentinel exists")))
+
+		By("Checking k3s is pointing to https")
+		Eventually(func() string {
+			out, _ := vm.Sudo("cat /etc/rancher/k3s/k3s.yaml")
+			return out
+		}, 900*time.Second, 10*time.Second).Should(ContainSubstring("https:"))
+
+		By("checking if logs are rotated")
+		out, err = vm.Sudo("logrotate -vf /etc/logrotate.d/kairos")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(out).To(ContainSubstring("log needs rotating"))
+		_, err = vm.Sudo("ls /var/log/kairos/agent-provider.log.1.gz")
+		Expect(err).ToNot(HaveOccurred())
+
+		By("wait system-upgrade-controller")
+		Eventually(func() string {
+			out, _ := kubectl(vm, "get pods -A")
+			return out
+		}, 900*time.Second, 10*time.Second).Should(ContainSubstring("system-upgrade-controller"))
+
+		By("wait for all containers to be in running state")
+		Eventually(func() string {
+			out, _ := kubectl(vm, "get pods -A")
+			fmt.Printf("out = %+v\n", out)
+			return out
+
+		}, 900*time.Second, 10*time.Second).ShouldNot(And(ContainSubstring("Pending"), ContainSubstring("ContainerCreating")))
+
+		By("applying upgrade plan")
+		err = vm.Scp("assets/suc.yaml", "./suc.yaml", "0770")
+		Expect(err).ToNot(HaveOccurred())
+
+		Eventually(func() string {
+			out, _ := kubectl(vm, "apply -f suc.yaml")
+			return out
+		}, 900*time.Second, 10*time.Second).Should(ContainSubstring("unchanged"))
+
+		Eventually(func() string {
+			out, _ = kubectl(vm, "get pods -A")
+			return out
+		}, 900*time.Second, 10*time.Second).Should(ContainSubstring("apply-os-upgrade-on-"), out)
+
+		Eventually(func() string {
+			out, _ = kubectl(vm, "get pods -A")
+			version, _ := vm.Sudo(getVersionCmd)
+			return version
+		}, 30*time.Minute, 10*time.Second).Should(ContainSubstring("v"), out)
+	})
+})
