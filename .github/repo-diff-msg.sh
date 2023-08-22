1e7309a0b596434afdbe0c682bc3a76d392c57ec
Dimitris Karakasilis <dimitris@karakasilis.me>
2023-08-22 10:47:28 +0300

    Fix linting errors (#1753)
    * Fix linting errors
    
    https://www.shellcheck.net/wiki/SC2028
    https://www.shellcheck.net/wiki/SC2129
    https://www.shellcheck.net/wiki/SC2005
    
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>
    
    * We don't need a literal `\n` but just a new line
    
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>
    
    ---------
    
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- .github/repo-diff-msg.sh
+++ .github/repo-diff-msg.sh
@@ -6,16 +6,22 @@ echo "Bump of Kairos repositories" > pr-message
 echo "--------------------------" >> pr-message
 DIFF=$(diff -u build/versions.old.yaml build/versions.new.yaml)
 if [[ $? == 1 ]]; then
-  echo "> [\!WARNING]" >> pr-message
-  echo "> There were changes to installed packages" >> pr-message
-  echo "\`\`\`diff" >> pr-message
-  echo "${DIFF}" >> pr-message
-  echo "\`\`\`" >> pr-message
-  echo "\n" >> pr-message
+  {
+    echo "> [\!WARNING]"
+    echo "> There were changes to installed packages"
+    echo "\`\`\`diff"
+    echo "${DIFF}"
+    echo "\`\`\`"
+    echo
+  } >> pr-message
 fi
-echo "> [\!IMPORTANT]" >> pr-message
-echo "> Full package list from new repo" >> pr-message
-echo "\`\`\`yaml" >> pr-message
-echo "$(cat build/versions.new.yaml)" >> pr-message
-echo "\`\`\`" >> pr-message
+
+{
+  echo "> [\!IMPORTANT]"
+  echo "> Full package list from new repo"
+  echo "\`\`\`yaml"
+  cat build/versions.new.yaml
+  echo "\`\`\`"
+} >> pr-message
+
 exit 0
