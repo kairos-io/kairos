661e18b7c1f8580b9319de13e3124afb5a8b105b
Itxaka <itxaka@kairos.io>
2023-10-24 14:20:49 +0200

    Debug
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- tests/upgrade_latest_cli_test.go
+++ tests/upgrade_latest_cli_test.go
@@ -59,7 +59,7 @@ var _ = Describe("k3s upgrade manual test", Label("upgrade-latest-with-cli"), fu
 			Expect(currentVersion).To(ContainSubstring("v"))
 
 			By(fmt.Sprintf("Upgrading to: %s", containerImage))
-			out, err := vm.Sudo("kairos-agent upgrade --force --image " + containerImage)
+			out, err := vm.Sudo("kairos-agent --debug upgrade --force --image " + containerImage)
 			Expect(err).ToNot(HaveOccurred(), string(out))
 			Expect(out).To(ContainSubstring("Upgrade completed"))
 			Expect(out).To(ContainSubstring(containerImage))
@@ -78,6 +78,7 @@ var _ = Describe("k3s upgrade manual test", Label("upgrade-latest-with-cli"), fu
 				return v
 				// TODO: Add regex semver check here
 			}, 30*time.Minute, 10*time.Second).ShouldNot(Equal(currentVersion))
+
 		})
 	})
 })
