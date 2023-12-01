722b2dfd2ab4f36b42e2485bc286009f2e73c878
Mauro Morales <mauro.morales@spectrocloud.com>
2023-12-01 08:10:35 +0100

    add missing which-init.sh file :facepalm:
    Signed-off-by: Mauro Morales <mauro.morales@spectrocloud.com>

---
+++ images/which-init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check for systemd
+if [ -x /bin/systemctl ] || [ -x /usr/bin/systemctl ]; then
+    echo "systemd"
+    exit 0
+fi
+
+# Check for OpenRC
+if [ -x /sbin/rc ] || [ -x /usr/sbin/rc ]; then
+    echo "openrc"
+    exit 0
+fi
+
+# If neither systemd nor OpenRC is found
+exit 1
