ee85536ea013ff5e5293bea6c63afa1ee0f45ea3
Itxaka <itxaka@kairos.io>
2024-09-23 12:27:14 +0200

    Fix tests :D
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- tests/install_target_test.go
+++ tests/install_target_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	. "github.com/spectrocloud/peg/matcher"
 	"os"
+	"strings"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -33,8 +35,6 @@ var _ = Describe("kairos install test different targets", Label("install-test-ta
 
 			cc := fmt.Sprintf(`#cloud-config
 install:
-  auto: true
-  reboot: true
   device: /dev/disk/by-uuid/%s
 
 stages:
@@ -47,7 +47,7 @@ stages:
 				`, uuid)
 
 			By("Using the following config")
-			fmt.Fprintf(GinkgoWriter, cc)
+			fmt.Fprint(GinkgoWriter, cc)
 
 			t, err := os.CreateTemp("", "test")
 			ExpectWithOffset(1, err).ToNot(HaveOccurred())
@@ -63,9 +63,51 @@ stages:
 			// Test that install works
 			By("installing kairos", func() {
 				out, err = vm.Sudo(`kairos-agent --debug manual-install /tmp/config.yaml`)
+				fmt.Fprint(GinkgoWriter, out)
 				Expect(err).ToNot(HaveOccurred(), out)
 				Expect(out).Should(ContainSubstring("Running after-install hook"))
 				vm.Sudo("sync")
+			})
+
+			By("waiting for VM to reboot", func() {
+				vm.Reboot()
+				vm.EventuallyConnects(1200)
+			})
+
+			By("checking that vm has rebooted to 'active'", func() {
+				Eventually(func() string {
+					out, _ := vm.Sudo("kairos-agent state boot")
+					return out
+				}, 40*time.Minute, 10*time.Second).Should(
+					Or(
+						ContainSubstring("active_boot"),
+					))
+			})
+
+			By("checking corresponding state", func() {
+				out, err := vm.Sudo("kairos-agent state")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("boot: active_boot"))
+				currentVersion, err := vm.Sudo(getVersionCmd)
+				Expect(err).ToNot(HaveOccurred(), currentVersion)
+
+				stateAssertVM(vm, "oem.mounted", "true")
+				stateAssertVM(vm, "oem.found", "true")
+				stateAssertVM(vm, "persistent.mounted", "true")
+				stateAssertVM(vm, "state.mounted", "true")
+				stateAssertVM(vm, "oem.type", "ext4")
+				stateAssertVM(vm, "persistent.type", "ext4")
+				stateAssertVM(vm, "state.type", "ext4")
+				stateAssertVM(vm, "oem.mount_point", "/oem")
+				stateAssertVM(vm, "persistent.mount_point", "/usr/local")
+				stateAssertVM(vm, "persistent.name", "/dev/vda")
+				stateAssertVM(vm, "state.mount_point", "/run/initramfs/cos-state")
+				stateAssertVM(vm, "oem.read_only", "false")
+				stateAssertVM(vm, "persistent.read_only", "false")
+				stateAssertVM(vm, "state.read_only", "true")
+				stateAssertVM(vm, "kairos.version", strings.ReplaceAll(strings.ReplaceAll(currentVersion, "\r", ""), "\n", ""))
+				stateContains(vm, "system.os.name", "alpine", "opensuse", "ubuntu", "debian")
+				stateContains(vm, "kairos.flavor", "alpine", "opensuse", "ubuntu", "debian")
 			})
 		})
 		It("Selects the correct disk if using label for target", func() {
@@ -75,8 +117,6 @@ stages:
 
 			cc := fmt.Sprintf(`#cloud-config
 install:
-  auto: true
-  reboot: true
   device: /dev/disk/by-label/%s
 
 stages:
@@ -105,8 +145,50 @@ stages:
 			By("installing kairos", func() {
 				out, err = vm.Sudo(`kairos-agent --debug manual-install /tmp/config.yaml`)
 				Expect(err).ToNot(HaveOccurred(), out)
+				fmt.Fprint(GinkgoWriter, out)
 				Expect(out).Should(ContainSubstring("Running after-install hook"))
 				vm.Sudo("sync")
+			})
+
+			By("waiting for VM to reboot", func() {
+				vm.Reboot()
+				vm.EventuallyConnects(1200)
+			})
+
+			By("checking that vm has rebooted to 'active'", func() {
+				Eventually(func() string {
+					out, _ := vm.Sudo("kairos-agent state boot")
+					return out
+				}, 40*time.Minute, 10*time.Second).Should(
+					Or(
+						ContainSubstring("active_boot"),
+					))
+			})
+
+			By("checking corresponding state", func() {
+				out, err := vm.Sudo("kairos-agent state")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(out).To(ContainSubstring("boot: active_boot"))
+				currentVersion, err := vm.Sudo(getVersionCmd)
+				Expect(err).ToNot(HaveOccurred(), currentVersion)
+
+				stateAssertVM(vm, "oem.mounted", "true")
+				stateAssertVM(vm, "oem.found", "true")
+				stateAssertVM(vm, "persistent.mounted", "true")
+				stateAssertVM(vm, "state.mounted", "true")
+				stateAssertVM(vm, "oem.type", "ext4")
+				stateAssertVM(vm, "persistent.type", "ext4")
+				stateAssertVM(vm, "state.type", "ext4")
+				stateAssertVM(vm, "oem.mount_point", "/oem")
+				stateAssertVM(vm, "persistent.mount_point", "/usr/local")
+				stateAssertVM(vm, "persistent.name", "/dev/vda")
+				stateAssertVM(vm, "state.mount_point", "/run/initramfs/cos-state")
+				stateAssertVM(vm, "oem.read_only", "false")
+				stateAssertVM(vm, "persistent.read_only", "false")
+				stateAssertVM(vm, "state.read_only", "true")
+				stateAssertVM(vm, "kairos.version", strings.ReplaceAll(strings.ReplaceAll(currentVersion, "\r", ""), "\n", ""))
+				stateContains(vm, "system.os.name", "alpine", "opensuse", "ubuntu", "debian")
+				stateContains(vm, "kairos.flavor", "alpine", "opensuse", "ubuntu", "debian")
 			})
 		})
 	})
