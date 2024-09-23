35eb6897e3111ad4294c53c8c21b1e9c2ad24e4b
Itxaka <itxaka@kairos.io>
2024-09-23 10:18:14 +0200

    [WIP] Test selecting disk by uuid
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- tests/install_test.go
+++ tests/install_test.go
@@ -68,6 +68,56 @@ var _ = Describe("kairos install test", Label("install-test"), func() {
 	})
 
 	Context("install", func() {
+		Context("Selecting the disk by uuid/label", func() {
+			It("Selects the correct disk if using uuid for target", func() {
+				expectSecureBootEnabled(vm)
+				// Get uuid of main disk
+				uuid, err := vm.Sudo("lsblk /dev/vda -o UUID -n")
+
+				cc := fmt.Sprintf(`#cloud-config
+				install:
+				  auto: true
+				  reboot: true
+				  device: /dev/disk/by-uuid/%s
+				
+				stages:
+				  initramfs:
+					- name: "Set user and password"
+					  users:
+						kairos:
+						  passwd: "kairos"
+					  hostname: kairos-{{ trunc 4 .Random }}
+				`, uuid)
+
+				By("Using the following config")
+				fmt.Fprintf(GinkgoWriter, cc)
+
+				t, err := os.CreateTemp("", "test")
+				ExpectWithOffset(1, err).ToNot(HaveOccurred())
+
+				defer os.RemoveAll(t.Name())
+				err = os.WriteFile(t.Name(), []byte(cc), os.ModePerm)
+				Expect(err).ToNot(HaveOccurred())
+
+				err = vm.Scp(t.Name(), "/tmp/config.yaml", "0770")
+				Expect(err).ToNot(HaveOccurred())
+
+				var out string
+				By("installing kairos", func() {
+					out, err = vm.Sudo(`kairos-agent --debug manual-install --device "auto" /tmp/config.yaml`)
+					Expect(err).ToNot(HaveOccurred(), out)
+					Expect(out).Should(ContainSubstring("Running after-install hook"))
+					vm.Sudo("sync")
+				})
+
+				By("waiting for VM to reboot", func() {
+					vm.Reboot()
+					vm.EventuallyConnects(1200)
+				})
+
+				expectSecureBootEnabled(vm)
+			})
+		})
 		It("cloud-config syntax mixed with extended syntax", func() {
 
 			expectSecureBootEnabled(vm)
